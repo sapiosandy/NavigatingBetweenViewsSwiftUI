85aa8e7fafcbbb2a635ba29d3d77a7ad76afcaf0
Sandy Gomez <sgomez1029@gmail.com>
2024-04-23 12:18:58 -0700

    Initial Commit

---
+++ NavigatingBetweenViewsSwiftUI/NavigatingBetweenViewsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigatingBetweenViewsSwiftUIApp.swift
+//  NavigatingBetweenViewsSwiftUI
+//
+//  Created by Sandra Gomez on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigatingBetweenViewsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
